9d255cff548044287c4527b76229512725d696f7
Oleksii Forostianov <143897315+oforo001@users.noreply.github.com>
2025-10-15 16:34:18 +0200

    Update data_quality_checks.sql

--- scripts/silver/data_quality_checks.sql
+++ scripts/silver/data_quality_checks.sql
@@ -1,4 +1,8 @@
 
+/*
+Test Module for silver.crm_cst_info table
+*/
+
 --Check for cst_id uniquest
 --Expected result: No result
 SELECT cst_id, COUNT(cst_id) as test
@@ -16,3 +20,40 @@ WHERE cst_firstname != TRIM(cst_firstname);
 --Expected result: 'n/a', 'Male', 'Female'
 SELECT DISTINCT cst_gndr
 FROM silver.crm_cust_info;
+
+
+
+
+/*
+Test Module for silver.crm_prd_info table
+*/
+
+use DataWarehouse
+
+SELECT prd_id,
+COUNT(*) AS dublicates
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+--checking of extra spaces
+--Expected output: No output
+SELECT
+	prd_nm
+FROM silver.crm_prd_info
+WHERE TRIM(prd_nm) != prd_nm;
+
+--checking negatie prices or NULL values
+-- Expected output: No output
+SELECT prd_cost
+FROM silver.crm_prd_info
+WHERE  prd_cost < 0 OR prd_cost IS NULL;
+
+--check the data issue with dates
+--Expected output: No output
+SELECT *
+FROM silver.crm_prd_info
+WHERE prd_start_dt > prd_end_dt;
+
+
+
