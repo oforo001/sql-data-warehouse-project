0dc54c272e0af4ce61fdd09fd31f4936a9360417
Oleksii Forostianov <143897315+oforo001@users.noreply.github.com>
2025-11-06 14:16:05 +0100

    Added silver.erp_px_cat_g1v2 table dml

--- scripts/silver/dml.sql
+++ scripts/silver/dml.sql
@@ -169,3 +169,23 @@ END AS ctry
 FROM bronze.erp_loc_a101;
 
 
+/*
+==========================
+Insertion into silver.erp_px_cat_g1v2
+==========================
+*/
+INSERT INTO silver.erp_px_cat_g1v2(
+id,
+cat,
+subcat,
+maintenance
+)
+
+SELECT
+id,
+cat,
+subcat,
+maintenance
+FROM bronze.erp_px_cat_g1v2;
+
+
