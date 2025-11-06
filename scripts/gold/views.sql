950903d66c036d06a9cefba2c346069d6c9e0052
Oleksii Forostianov <143897315+oforo001@users.noreply.github.com>
2025-11-06 16:34:36 +0100

    Create views.sql

---
+++ scripts/gold/views.sql
@@ -0,0 +1,17 @@
+
+SELECT
+ci.cst_id,
+ci.cst_key,
+ci.cst_firstname,
+ci.cst_lastname,
+ci.cst_marital_status,
+ci.cst_gndr,
+ci.cst_create_date,
+ca.bdate,
+ca.gen,
+la.ctry
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+ON        ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+ON        ci.cst_key = la.cid;
