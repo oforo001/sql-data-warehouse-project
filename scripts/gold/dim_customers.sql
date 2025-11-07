42f2236fb3e9a6e6f344d318234506d5885d184a
Oleksii Forostianov <143897315+oforo001@users.noreply.github.com>
2025-11-07 20:21:50 +0100

    Added: gold.dim_customers

---
+++ scripts/gold/dim_customers.sql
@@ -0,0 +1,20 @@
+CREATE VIEW gold.dim_customers AS
+SELECT
+ROW_NUMBER() OVER (ORDER BY ci.cst_id) as customer_key,
+ci.cst_id AS customer_id,
+ci.cst_key AS customer_number,
+ci.cst_firstname AS first_name,
+ci.cst_lastname AS last_name,
+la.ctry AS contry,
+ci.cst_marital_status AS marital_status,
+CASE	
+	WHEN ci.cst_gndr <> NULL THEN ci.cst_gndr
+	ELSE COALESCE(ca.gen, 'n/a')
+END AS gender,
+ca.bdate AS bithdate,
+ci.cst_create_date AS create_date
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+ON        ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+ON        ci.cst_key = la.cid;
